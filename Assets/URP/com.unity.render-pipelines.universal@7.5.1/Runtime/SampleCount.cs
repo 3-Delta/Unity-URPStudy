a5ea2757c5c2921d7610e338e8a31cd6b1d5f78a
cignal <cignal@users.noreply.github.com>
2021-09-27 00:28:22 +0800

    提交urp

---
+++ Assets/URP/com.unity.render-pipelines.universal@7.5.1/Runtime/SampleCount.cs
@@ -0,0 +1,11 @@
+using UnityEngine.Scripting.APIUpdating;
+
+namespace UnityEngine.Rendering.Universal
+{
+      public enum SampleCount
+    {
+        One = 1,
+        Two = 2,
+        Four = 4,
+    }
+}
